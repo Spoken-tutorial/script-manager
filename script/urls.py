90f760734df8992f8365b7fbbe63d6466a800987
abhijitbonik <abhijitbonik@hotmail.com>
2021-07-22 21:34:49 +0530

    change passwor

--- script/urls.py
+++ script/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('change-password/', auth_views.PasswordChangeView.as_view(), name='change_password'),
     path('scripts/', include('scriptmanager.urls', namespace='scripts')),
     re_path(r'', index, name='home')
 ]
