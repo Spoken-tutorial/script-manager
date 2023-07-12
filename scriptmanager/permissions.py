b47d67f56bd8f80b1d2d6a618fcaaf0e7f349bef
Sunil Shetye <sunilshetye@rocketmail.com>
2023-07-12 17:58:20 +0530

    fix pep8 errors and warnings

--- scriptmanager/permissions.py
+++ scriptmanager/permissions.py
@@ -2,80 +2,86 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .utils import get_roles
 from .models import ScriptDetail, Script
 
+
 class ScriptOwnerPermission(IsAuthenticatedOrReadOnly):
-  def has_permission(self, request, view):
-    if request.user.is_authenticated:
-      domain = view.kwargs['domain']
-      fid = view.kwargs['fid']
-      lid = view.kwargs['lid']
-      return  is_Contributor(domain, fid, lid, request.user.username) or is_DomainReviewer(domain, fid, lid, request.user.username) or is_QualityReviewer(domain, fid, lid, request.user.username)
-    if request.user.is_anonymous:
-      domain = view.kwargs['domain']
-      fid = view.kwargs['fid']
-      lid = view.kwargs['lid']
-      tid = view.kwargs['tid']
-      vid = view.kwargs['vid']
-      return Script.objects.filter(domain=domain, foss_id=fid, language_id=lid, tutorial_id=tid, versionNo=vid, status=True).exists()
-    return False
-      # return True
-    
-  def has_object_permission(self, request, view, obj):
-    obj.user == request.user
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            domain = view.kwargs['domain']
+            fid = view.kwargs['fid']
+            lid = view.kwargs['lid']
+            return is_Contributor(domain, fid, lid, request.user.username) or is_DomainReviewer(domain, fid, lid, request.user.username) or is_QualityReviewer(domain, fid, lid, request.user.username)
+        if request.user.is_anonymous:
+            domain = view.kwargs['domain']
+            fid = view.kwargs['fid']
+            lid = view.kwargs['lid']
+            tid = view.kwargs['tid']
+            vid = view.kwargs['vid']
+            return Script.objects.filter(domain=domain, foss_id=fid, language_id=lid, tutorial_id=tid, versionNo=vid, status=True).exists()
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        obj.user == request.user
+
 
 class ScriptModifyPermission(IsAuthenticatedOrReadOnly):
-  def has__permission(self, request, view):
-    obj = ScriptDetail.objects.get(pk=view.kwargs['script_detail_id'])
-    domain=obj.script.domain
-    fid=obj.script.fid
-    lid=obj.script.lid
-    if request.user.is_authenticated:
-      return  obj.script.user==request.user or is_Contributor(domain, fid, lid, request.user.username) or is_DomainReviewer(domain, fid, lid, request.user.username) or is_QualityReviewer(domain, fid, lid, request.user.username)
-    return False
-      
+    def has__permission(self, request, view):
+        obj = ScriptDetail.objects.get(pk=view.kwargs['script_detail_id'])
+        domain = obj.script.domain
+        fid = obj.script.fid
+        lid = obj.script.lid
+        if request.user.is_authenticated:
+            return obj.script.user == request.user or is_Contributor(domain, fid, lid, request.user.username) or is_DomainReviewer(domain, fid, lid, request.user.username) or is_QualityReviewer(domain, fid, lid, request.user.username)
+        return False
+
+
 class PublishedScriptPermission(IsAuthenticatedOrReadOnly):
-    
-  def has_object_permission(self, request, view, obj):
-    if request.user.is_authenticated:
-      domain = view.kwargs['domain']
-      fid = view.kwargs['fid']
-      lid = view.kwargs['lid']
-      return  is_Contributor(domain, fid, lid, request.user.username) or is_DomainReviewer(domain, fid, lid, request.user.username) or is_QualityReviewer(domain, fid, lid, request.user.username)
-    return False
-  
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated:
+            domain = view.kwargs['domain']
+            fid = view.kwargs['fid']
+            lid = view.kwargs['lid']
+            return is_Contributor(domain, fid, lid, request.user.username) or is_DomainReviewer(domain, fid, lid, request.user.username) or is_QualityReviewer(domain, fid, lid, request.user.username)
+        return False
+
+
 class ReviewScriptPermission(IsAuthenticatedOrReadOnly):
-  def has_object_permission(self, request, view, obj):
-    if request.user.is_authenticated:
-      domain = view.kwargs['domain']
-      fid = view.kwargs['fid']
-      lid = view.kwargs['lid']
-      return  is_DomainReviewer(domain, fid, lid, request.user.username) or is_QualityReviewer(domain, fid, lid, request.user.username)
-    return False
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated:
+            domain = view.kwargs['domain']
+            fid = view.kwargs['fid']
+            lid = view.kwargs['lid']
+            return is_DomainReviewer(domain, fid, lid, request.user.username) or is_QualityReviewer(domain, fid, lid, request.user.username)
+        return False
 
 
 class CommentOwnerPermission(IsAuthenticatedOrReadOnly):
-  def has_object_permission(self, request, view, obj):
-    return request.user.is_authenticated
-    
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated
+
 
 class CanCommentPermission(IsAuthenticatedOrReadOnly):
-  def has_object_permission(self, request, view, obj):
-    return not obj.script.status
+    def has_object_permission(self, request, view, obj):
+        return not obj.script.status
+
 
 class CanRevisePermission(IsAuthenticatedOrReadOnly):
-  def has_object_permission(self, request, view, obj):
-    return not obj.script.status
+    def has_object_permission(self, request, view, obj):
+        return not obj.script.status
+
 
 def is_Contributor(domain, fid, lid, username):
-  roles = get_roles(domain, fid, lid, username)
-  print(" TEST *********  16 roles : {}".format(roles))
-  return 'Contributor' in roles
+    roles = get_roles(domain, fid, lid, username)
+    print(" TEST *********  16 roles : {}".format(roles))
+    return 'Contributor' in roles
+
 
 def is_DomainReviewer(domain, fid, lid, username):
-  roles = get_roles(domain, fid, lid, username)
-  print(" TEST *********  16 roles : {}".format(roles))
-  return 'Domain-Reviewer' in roles
+    roles = get_roles(domain, fid, lid, username)
+    print(" TEST *********  16 roles : {}".format(roles))
+    return 'Domain-Reviewer' in roles
+
 
 def is_QualityReviewer(domain, fid, lid, username):
-  roles = get_roles(domain, fid, lid, username)
-  print(" TEST *********  16 roles : {}".format(roles))
-  return 'Quality-Reviewer' in roles
+    roles = get_roles(domain, fid, lid, username)
+    print(" TEST *********  16 roles : {}".format(roles))
+    return 'Quality-Reviewer' in roles
